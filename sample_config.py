7a3a403b068286c1a0adef48a6bc40c0cb420084
twomax1177 <95082796+twomax1177@users.noreply.github.com>
2021-11-26 15:01:10 +0200

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -15,13 +15,13 @@ class Config(object):
     # Get a bot token from botfather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "2130767985:AAG9uZyT6d4C94emicOThIYYyJFPxYYTAj0")
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "3911505"))
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "8d055b5e3fb8a249f09cb1b5d7ba3040")
     # Database URI
     DB_URI = os.environ.get("DATABASE_URL", "")
     # List of admin user ids for special functions(
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "@twomax").split())
 
 
 def LOGGER(name: str) -> logging.Logger:
